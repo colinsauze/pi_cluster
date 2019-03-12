7a354ff303af59e96dabf058f6d3f8ac3fb8c5e0
Colin Sauze <cos@aber.ac.uk>
2019-03-12 12:01:24 +0000

    changing directory so output appears in the right place

--- mpi_pi_demo/mpi_numpi.sh
+++ mpi_pi_demo/mpi_numpi.sh
@@ -2,8 +2,9 @@
 
 ###
 #SBATCH --job-name=calculate_pi
-#SBATCH --output=out.%J
+#SBATCH --output="Job %J"
 #SBATCH --error=err.%J
 #SBATCH --ntasks=10
 ###
+cd /home/pi/pi_cluster/mpi_pi_demo
 mpirun python3 mpi_numpi.py $1
