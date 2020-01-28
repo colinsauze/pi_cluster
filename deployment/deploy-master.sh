a5e85c1b8acf306693640812801c97a70fbda69e
Colin Sauze <cos@aber.ac.uk>
2020-01-28 15:58:07 +0000

    fixes for python and hostnames

--- deployment/deploy-master.sh
+++ deployment/deploy-master.sh
@@ -18,10 +18,15 @@ cd /home/pi/pi_cluster/config
 
 cp -r master/etc/* /etc
 cp master/config.txt /boot
+echo "master" > /etc/hostname
+
 
 #tmux wants the US locale??? Seems to work without it, commenting out for now
 #echo "enable en_US UTF8 locale"
 #dpkg-reconfigure locales
+
+#disable trying to get DHCP leases
+systemctl disable dhcpcd
 
 #enable SSHD
 systemctl enable ssh
@@ -29,6 +34,11 @@ systemctl enable ssh
 #enable hostapd
 systemctl unmask hostapd
 systemctl enable hostapd
+
+#enable slurm
+systemctl enable munge
+systemctl enable slurmctld
+
 
 #set the password to "Raspberries"
 usermod -p '6$YRfpaLjt1qWOJAPd$dpGwjm0AxrRmDmKhfpjPWnk441gpO5ESUCj5LmEkAxon.VRbhldH7JQGj9uO0nvLnBechI9HK.FtZosuB6upR0' pi
@@ -47,28 +57,46 @@ ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime
 
 #setup python3 module
 
-cd /home/pi
+#python needs ssl for pip and the system version of openssl seems to incompatible
+#i'm still struggling to find a version which actually works
+#cd /home/pi
+#wget https://www.openssl.org/source/old/1.0.2/openssl-1.0.2u.tar.gz
+#tar xvfz openssl-1.0.2u.tar.gz
+#cd  openssl-1.0.2u
+#./config --prefix=/usr/share/modules/Modules/python/3.7.0
+#make -j 4
+#make install
+
+
 wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
 tar xvfz Python-3.7.0.tgz 
 cd Python-3.7.0/
+#LDFLAGS="-L/usr/share/modules/Modules/python/3.7.0/lib -L/usr/lib -L/lib"
+#./configure --prefix=/usr/share/modules/Modules/python/3.7.0 --with-openssl=/usr/share/modules/Modules/python/3.7.0
 ./configure --prefix=/usr/share/modules/Modules/python/3.7.0 --with-ssl
 make -j4
 make install
 
 #remove default system modules
 mv /usr/share/modules/modulefiles /usr/share/modules/modulefiles.unwanted
-mkdir -p /usr/share/modules/modulefiles
+mkdir -p /usr/share/modules/modulefiles/python/3.7.0 
 cp /home/pi/pi_cluster/modules/python-3.7.0 /usr/share/modules/modulefiles/python/3.7.0 
 
-## Installing pip packages systemwide
+
+# Using pip in the module is broken because of SSL, using the system pip seems to still install things in places the module can find
+pip3 install cython
+
+#load the module to build numpy
 . /etc/profile.d/modules.sh 
 module load python/3.7.0
-pip3 install cython
 
 cd /home/pi
 wget https://github.com/numpy/numpy/releases/download/v1.17.4/numpy-1.17.4.tar.gz
 tar xvfz numpy-1.17.4.tar.gz
 cd numpy-1.17.4
 python3.7 setup.py build -j 4 install --prefix /usr/share/modules/Modules/python/3.7.0/
-       
+
+#unload the module to install matplotlib using the system pip 
+module purge
+
 pip3 install matplotlib
